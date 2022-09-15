5ff57d9d2b820217df73855cbb2c7fde5c321739
Burak <109901413+quibblees@users.noreply.github.com>
2022-09-15 16:26:14 +0300

    OgrenciBilgiSistemi

---
+++ src/Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher {
+    String name;
+    String phoneNumber;
+    String branch;
+
+    Teacher(String name, String phoneNumber, String branch) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.branch = branch;
+    }
+
+    public void print() {
+        System.out.println("========================================");
+        System.out.println("Teacher added to the system");
+        System.out.println("Teacher's name : " + this.name);
+        System.out.println("Teacher's number : " + this.phoneNumber);
+        System.out.println("Teacher's branch : " + this.branch);
+    }
+}
