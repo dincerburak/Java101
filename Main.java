610c7babf118dbb4c819356b94b1c1944a34fffc
Burak <109901413+quibblees@users.noreply.github.com>
2022-09-16 13:04:37 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,15 @@
+package BoksOyunu;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+
 public class Main {
     public static void main(String[] args) {
 
-        Course mathematics = new Course("Mathematics", "101", "MATH");
-        Course physics = new Course("Physics", "202", "PHYSICS");
-        Course english = new Course("English", "303", "ENG");
+    Fighter fighter1 = new Fighter("Mete", 100, 105, 14, 50);
+    Fighter fighter2 = new Fighter("Burak", 95, 120, 16, 40);
 
-        Teacher teacher1 = new Teacher("Burak", "444", "MATH");
-        Teacher teacher2 = new Teacher("Mete", "333", "PHYSICS");
-        Teacher teacher3 = new Teacher("Eda", "222", "ENG");
-
-        mathematics.addTeacher(teacher1);
-        physics.addTeacher(teacher2);
-        english.addTeacher(teacher3);
-
-        Student student1 = new Student("Mustafa", "622", 11, mathematics, physics, english);
-        student1.addBulkmidtermNote(87,73,91);
-        student1.addBulkfinalNote(82,81,88);
-        student1.isPass();
-
-        Student student2 = new Student("Fuat", "631", 11, mathematics, physics, english);
-        student2.addBulkmidtermNote(92, 63, 55);
-        student2.addBulkfinalNote(84, 79, 66);
-        student2.isPass();
-
-        Student student3 = new Student("Mesut", "627", 11, mathematics, physics, english);
-        student3.addBulkmidtermNote(43,23,87);
-        student3.addBulkfinalNote(55, 30, 65);
-        student3.isPass();
-
-
+    Match match = new Match(fighter1,fighter2 ,90,110);
+        match.run();
     }
 }
